0f2b2154e022a8028410e9d419254a382dffcd02
yuda-lyu <firsemisphere@gmail.com>
2026-07-23 18:22:30 +0800

    modify: update pks

--- toolg/gDistRollup.mjs
+++ toolg/gDistRollup.mjs
@@ -5,24 +5,13 @@ import rollupFiles from 'w-package-tools/src/rollupFiles.mjs'
 let fdSrc = './src'
 let fdTar = './dist'
 
-async function core() {
-
-    await rollupFiles({ //rollupFiles預設會clean folder
-        fns: 'WFft.mjs',
-        fdSrc,
-        fdTar,
-        nameDistType: 'kebabCase',
-        globals: {
-        },
-        external: [
-        ],
-    })
-        .catch((err) => {
-            console.log(err)
-        })
-
-}
-core()
-    .catch((err) => {
-        console.log(err)
-    })
+rollupFiles({ //rollupFiles預設會clean folder
+    fns: 'WFft.mjs',
+    fdSrc,
+    fdTar,
+    nameDistType: 'kebabCase',
+    globals: {
+    },
+    external: [
+    ],
+})
